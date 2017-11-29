b5ee925295dba5887bcb663c1ac12980ce7bfcb1
WeiAnAn <j22461615@gmail.com>
2017-11-29 16:57:33 +0800

    add privacy link

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MessengerBot, MemorySessionStore } = require('bottender');
 const { createServer } = require('bottender/express');
 const handler = require('./handler/messengerHandler');
+const path = require('path');
 
 const bot = new MessengerBot({
   accessToken: process.env.accessToken,
@@ -14,6 +15,10 @@ bot.setInitialState({
 });
 
 const server = createServer(bot);
+
+server.get('/privacy', (req, res) => {
+  return res.sendFile(path.resolve('./privacy.html'));
+});
 
 server.listen(process.env.PORT || 5000, () => {
   console.log(`server is running on ${process.env.PORT || 5000} port...`);
