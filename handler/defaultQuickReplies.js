f880418d8692d76699478a484b170e870a267fdc
WeiAnAn <j22461615@gmail.com>
2017-11-29 15:35:23 +0800

    quick replies template

---
+++ handler/defaultQuickReplies.js
@@ -0,0 +1,21 @@
+module.exports = {
+  quick_replies: [
+    {
+      content_type: 'text',
+      title: '基本資料',
+      payload: 'PERSONAL_INFORMATION'
+    }, {
+      content_type: 'text',
+      title: '領導及工作經驗',
+      payload: 'WORK_LEADER_EXPERIENCE'
+    }, {
+      content_type: 'text',
+      title: '參與過的專案',
+      payload: 'PARTICIPATE_PROJECT'
+    }, {
+      content_type: 'text',
+      title: '教學經驗',
+      payload: 'TEACHING_EXPERIENCE'
+    }
+  ]
+};
